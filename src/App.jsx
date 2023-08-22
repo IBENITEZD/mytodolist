80e3f54e013c216b874292f47bcefa564e75fad0
DoxaSistemas <ivan@doxasistemas.com>
2023-08-21 23:18:06 -0500

    Add ListItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, {Fragment ,useState} from "react";
 import { TodoList } from "./components/TodoList";
+import { v4 as uuidv4 } from 'uuid';
 
 export function App() {
      const [todos, setTodos] =  useState([
-          {id: 1, task: "Tarea 1", completed: false },
-     ])
+          { id: 1, task: "Tarea", completed: false },      
+     ]);
+
      return (
           <Fragment>
                <TodoList todos={todos} />
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { TodoItem } from './TodoItem';
 
 export function TodoList({ todos }) {
   return (
     <ul>
         {todos.map((todo) => (
-            <li>Tarea</li>
-            ))}
+            <TodoItem key={todo.id} todo={todos} /> 
+        ))}
     </ul>
   );
 }
