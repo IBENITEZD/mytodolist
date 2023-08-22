771d1956bac8651f8b480862953616b71bbd259d
DoxaSistemas <ivan@doxasistemas.com>
2023-08-21 21:57:33 -0500

    Lista de Tareas

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function TodoList({ todos }) {
+  return (
+    <ul>
+        {todos.map((todo) => (
+            <li>Tarea</li>
+            ))}
+    </ul>
+  );
+}
+
